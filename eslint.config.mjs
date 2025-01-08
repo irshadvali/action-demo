11e0e57277e3189b376282ebe6e1da6a2657ebeb
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2025-01-08 22:38:45 +0530

    test17

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,27 @@
-export default {
-  // ...other config
- 
-  rules: {
-      semi: ["warn", "always"]
+// eslint.config.mjs (or .js)
+import { createConfig } from 'eslint';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    parser: '@babel/eslint-parser', // Ensure that Babel can parse JSX and modern JS
+    parserOptions: {
+      requireConfigFile: false,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['react'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    rules: {
+      'react/jsx-uses-react': 'off', // React 17+ doesn't require the 'import React' in files
+      'react/react-in-jsx-scope': 'off', // For React 17+ JSX doesn't need this import
+    },
   },
-  // ...other config
-};
+];
