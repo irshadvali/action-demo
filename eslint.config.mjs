e5dad784012a7f162f4dba5672d3f561a8b20895
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2025-01-08 22:57:44 +0530

    test18

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
-
 export default [
   {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true, // Enable JSX syntax
+      },
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
+    },
     rules: {
-      'react/jsx-uses-react': 'off', // React 17+ doesn't require the 'import React' in files
-      'react/react-in-jsx-scope': 'off', // For React 17+ JSX doesn't need this import
+      'react/jsx-uses-react': 'off', // For React 17+
+      'react/react-in-jsx-scope': 'off', // For React 17+
     },
   },
 ];
