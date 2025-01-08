15dd98f092cff7a509fe21e695b025462f438d94
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2025-01-08 21:09:40 +0530

    test4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const sayHello = () => {
+  //   alert("Hello!")
+  // };
   return (
     <div>
       <p>hi</p>
       <p>Irshad Vali</p>
+      <button onClick={sayHello}>Say Hello</button>
     </div>
   );
 }
