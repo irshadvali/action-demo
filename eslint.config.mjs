0dfaf801ff986122e6c3d1f48507bf74ba37be5c
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2025-01-09 13:46:41 +0530

    test22

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,25 @@
 import reactPlugin from 'eslint-plugin-react';
+import babelParser from '@babel/eslint-parser';
 
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    },
     plugins: {
       react: reactPlugin,
     },
     rules: {
       'react/jsx-uses-react': 'off', // For React 17+
       'react/react-in-jsx-scope': 'off', // For React 17+
+      'react/prop-types': 'off', // Disable prop-types rule if using TypeScript
     },
   },
 ];
